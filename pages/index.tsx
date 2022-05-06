50bddcd19a97d0477085b18a4d13bd8ae65998ad
westprophet <westprophet@gmail.com>
2022-05-06 15:42:15 +0300

    wp

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import IndexPage from "../src/pages/IndexPage";
+
+const Home: NextPage = () => {
+  return (<IndexPage/>)
+}
+
+export default Home
