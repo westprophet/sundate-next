11f6939ec2b95a1040d719de289a8b1c7c4c7577
westprophet <westprophet@gmail.com>
2022-10-15 18:15:27 +0300

    next-seo ++

--- next-seo.config.js
+++ next-seo.config.js
@@ -7,15 +7,43 @@ export default {
     description:
       'SunDate is a new vision of online datings to make you Never alone in offline world',
     type: 'website',
+    site_name: 'sundate.app',
     images: [
+      {
+        url: `https://sundate.app/images/og_1010x907.jpg`,
+        width: 1010,
+        height: 709,
+        type: 'image/jpeg',
+        alt: 'sundate.app',
+      },
+      {
+        url: `https://sundate.app/images/og_264x268.jpg`,
+        width: 264,
+        height: 268,
+        type: 'image/jpeg',
+        alt: 'sundate.app',
+      },
+      {
+        url: `https://sundate.app/images/og_1010x907.png`,
+        width: 1010,
+        height: 709,
+        type: 'image/png',
+        alt: 'sundate.app',
+      },
+      {
+        url: `https://sundate.app/images/og_264x268.png`,
+        width: 264,
+        height: 268,
+        type: 'image/png',
+        alt: 'sundate.app',
+      },
       {
         url: `https://sundate.app/logo-min.webp`,
         width: 1010,
         height: 709,
-        type: 'application/svg+xml',
+        type: 'image/webp',
         alt: 'sundate.app',
       },
     ],
-    site_name: "sundate.app'",
   },
 };
