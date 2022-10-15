dcc3ecec4f56c653ca7b17501c9e5577bde4623b
westprophet <westprophet@gmail.com>
2022-10-15 17:55:23 +0300

    next-seo.config.js add

---
+++ next-seo.config.js
@@ -0,0 +1,21 @@
+export default {
+  title: 'SunDate',
+  description: 'SunDate is a new vision of online datings to make you Never alone in offline world',
+  openGraph: {
+    url: 'https://sundate.app',
+    title: 'SunDate',
+    description:
+      'SunDate is a new vision of online datings to make you Never alone in offline world',
+    type: 'website',
+    images: [
+      {
+        url: `https://sundate.app/logo-min.webp`,
+        width: 1010,
+        height: 709,
+        type: 'application/svg+xml',
+        alt: 'sundate.app',
+      },
+    ],
+    site_name: "sundate.app'",
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,14 @@ import '../src/scss/index.scss';
 import type { AppProps } from 'next/app';
 import theme from '../mui-theme';
 import { ThemeProvider } from '@mui/material';
+import { DefaultSeo } from 'next-seo';
+
+import SEO from '../next-seo.config';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </ThemeProvider>
   );
